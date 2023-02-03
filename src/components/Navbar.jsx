68f9de7c1d60d2a357fb516c8de0eb5573cea975
Ismoil Ortikov <cupcakestarsc325@gmail.com>
2023-02-03 09:57:42 +0500

    nav added and logo added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='h-37'>
+        <img className='h-37 w-32'  src="https://www.culta.io/hubfs/Cookies%20%281%29.jpg" alt="" />
+      
+    </nav>
+  )
+}
+
+export default Navbar
